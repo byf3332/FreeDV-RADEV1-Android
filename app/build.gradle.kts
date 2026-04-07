dfa0ee6ae64132b7d7fa1dd695249a748d33fb2f
byf3332 <71202267+byf3332@users.noreply.github.com>
2026-04-07 23:26:35 +0800

    update version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.byf3332.radexcvr"
         minSdk = 27
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         ndk {
             abiFilters += "arm64-v8a"
